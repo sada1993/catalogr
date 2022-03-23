f6788d73ec7a2a2ceede56c4e58e2dee95a3653f
Akash Sadashivapeth <akash.sadashivpeth@gmail.com>
2022-03-22 23:00:55 -0500

    update the default catalogr read path.

--- R/read_catalog.R
+++ R/read_catalog.R
@@ -2,6 +2,6 @@
 #' @description A convenient yaml wrapper to read a catalog yaml file that is saved through `caralogr()`
 #' @param yaml_filepath the filepath of the saved catalog file
 #' @export
-read_catalog <- function(yaml_filepath = getwd()){
+read_catalog <- function(yaml_filepath = file.path(getwd(), "catalogr.yaml")){
   yaml::read_yaml(yaml_filepath)
 }
